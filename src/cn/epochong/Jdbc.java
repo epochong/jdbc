ed3da7fbeae22a9ea86a94263ba1a7afad658c6f
epochong <Valentineone@example.com>
2019-05-22 05:25:22 +0800

    建立了java和数据库的连接，解决了因为mysql版本过高而导致的连接失败

--- src/cn/epochong/Jdbc.java
+++ src/cn/epochong/Jdbc.java
@@ -7,15 +7,35 @@ package cn.epochong;
  * @describe
  */
 
+import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 /**
  * JDBC入门
  */
 public class Jdbc {
     public static void main(String[] args) throws Exception{
-        Class.forName("com.mysql.jdbc.Driver");
-        DriverManager.getConnection("jdbc:mysql://localhost:3306/","root","password");
-        String sql = "update account set balance = ";
-    }
+            //1. 导入驱动jar包
+            //2.注册驱动
+            // Class.forName("com.mysql.jdbc.Driver");
+            //3.获取数据库连接对象
+            //Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db3", "root", "root");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/jdbc", "root", "wangchong");
+            //4.定义sql语句
+            //String sql = "update account set balance = 2000 where id = 1";
+            String updateSql = "update jdbc.tb_user set mname = 'Jack' where id = 1";
+            String insertSql = "insert into tb_user values(3,'Baby','女','123@qq.com,'199-01-01')";
+            //5.获取执行sql的对象 Statement
+            Statement stmt = conn.createStatement();
+            //6.执行sql
+            int count = stmt.executeUpdate(updateSql);
+            //7.处理结果
+            System.out.println("执行修改语句:" + "影响行数:" + count + "行");
+            //stmt.executeUpdate()
+
+            //8.释放资源
+            stmt.close();
+            conn.close();
+        }
 }
